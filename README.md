1099becacad3bfd3514a1ff721ed5c07895c8797
KaceyStarr <thediamondswordnews@yahoo.com>
2025-07-05 21:43:27 -0500

    Test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # utrgvcs-schedule-optimizer
 A web application built to help UTRGV computer science students make smarter, more informed course selections by leveraging real student feedback.
+
+backend
+cd server
+node server.js
+
+frontend
+cd client
+npm start
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get('/api', (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
-});
+const express = require('express') // Import express 
+const cors = require('cors') // Import cors for cross-origin resource sharing
+const app = express() // Create an instance of express
+const port = 5000 // Define the port to listen on
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(cors()) // Use cors middleware to allow cross-origin requests
 
 
+//Hello World Test function
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Create a route to handle the POST request
+app.listen(port, () => {})
+console.log(`Server is running on http://localhost:${port}`) // Log the server URL to the console
